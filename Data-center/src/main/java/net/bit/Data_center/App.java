69494a3fe355be8b353b3bbde838eab8ff482891
bitweded <bitwededyosef@gmail.com>
2019-06-30 12:34:56 +0300

    set up complited

---
+++ Data-center/src/main/java/net/bit/Data_center/App.java
@@ -0,0 +1,13 @@
+package net.bit.Data_center;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
